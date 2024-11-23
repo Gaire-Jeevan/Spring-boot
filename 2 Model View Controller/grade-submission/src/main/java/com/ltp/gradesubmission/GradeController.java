cda7aa30e779935314afcd286c9a272e8f52696d
Jeevan <gairejeevan7@gmail.com>
2024-11-23 14:27:45 +0530

    variable expression

--- 2 Model View Controller/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 2 Model View Controller/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,13 +1,17 @@
 package com.ltp.gradesubmission;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
     
     @GetMapping("/grades")
-    public String sayGrades() {
+    public String sayGrades(Model model) {
+        Grade grade = new Grade("Harry", "Portions", "C-");
+        model.addAttribute("grade", grade);
+
         return "grades";
     }
 }
