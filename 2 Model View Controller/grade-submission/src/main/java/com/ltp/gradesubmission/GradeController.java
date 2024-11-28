df90d0508d44bad59f1323921606584a6a507ee6
Jeevan <gairejeevan7@gmail.com>
2024-11-28 22:33:40 +0530

    Thymeleaf loop

--- 2 Model View Controller/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 2 Model View Controller/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -4,14 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
 @Controller
 public class GradeController {
-    
+
+    // List<Grade> studentGrades = new ArrayList<>(); as directly populating arraylist cause the duplicate row when we refresh our page
+    List<Grade> studentGrades = Arrays.asList(
+            new Grade("Harry", "Potions", "C-"),
+            new Grade("Hermione", "Arithmancy", "A+"),
+            new Grade("Neville", "Charms", "A-")
+    );
+
     @GetMapping("/grades")
     public String sayGrades(Model model) {
-        Grade grade = new Grade("Harry", "Portions", "C-");
-        model.addAttribute("grade", grade);
+//        this is required when we populate arraylist directly
 
+//        studentGrades.add(new Grade("Harry", "Potions", "C-"));
+//        studentGrades.add(new Grade("Hermione", "Arithmancy", "A+"));
+//        studentGrades.add(new Grade("Neville", "Charms", "A-"));
+
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 }
