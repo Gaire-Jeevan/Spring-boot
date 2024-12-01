20548d051e6598c5f018da42c5dae1eca7de0b9b
Jeevan <gairejeevan7@gmail.com>
2024-12-02 00:53:35 +0530

    update Student grade with the help of id

--- 2 Model View Controller/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 2 Model View Controller/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -15,11 +15,11 @@ import java.util.ArrayList;
 public class GradeController {
 
     // List<Grade> studentGrades = new ArrayList<>(); as directly populating arraylist cause the duplicate row when we refresh our page
-    List<Grade> studentGrades = Arrays.asList(
-            new Grade("Harry", "Potions", "C-"),
-            new Grade("Hermione", "Arithmancy", "A+"),
-            new Grade("Neville", "Charms", "A-")
-    );
+//    List<Grade> studentGrades = Arrays.asList(
+//            new Grade("Harry", "Potions", "C-"),
+//            new Grade("Hermione", "Arithmancy", "A+"),
+//            new Grade("Neville", "Charms", "A-")
+//    );
 
 //    to update field after user submit
     List<Grade> studentGrade = new ArrayList<>();
@@ -38,41 +38,42 @@ public class GradeController {
     }
 
     @PostMapping("/handleSubmit")
-    public String submitGrade(Grade grade) {
+    public String submitForm(Grade grade) {
 //        System.out.println(grade);
 
 //        if user not exist then only add
-        int index = getGradeIndex(grade.getName());
-        if (index == -1000) {
+        int index = getGradeIndex(grade.getId());
+        if (index == Constants.NOT_FOUND) {
             studentGrade.add(grade);
         }
         else {
+//            if id is present then update in same index
             studentGrade.set(index, grade);
         }
 //        first out springboot create empty grade object using our empty constructor and update every field using setter with the help of payload of POST request
-        studentGrade.add(grade);
+//        studentGrade.add(grade);
 
         return "redirect:/grades";
     }
 
     @GetMapping("/")
-    public String getForm(Model model, @RequestParam(required = false) String name) {
+    public String getForm(Model model, @RequestParam(required = false) String id) {
 //        this string name is here because it fetch the data from url to access form
 //        System.out.println(name);
 
-
+          int index = getGradeIndex(id);
 //        bind entire form to a model object
-        model.addAttribute("grade", getGradeIndex(name) == -1000 ? new Grade() : studentGrade.get(getGradeIndex(name)));
+        model.addAttribute("grade", index == Constants.NOT_FOUND ? new Grade() : studentGrade.get(index));
 
         return "form";
     }
 
-    public Integer getGradeIndex(String name) {
+    public Integer getGradeIndex(String id) {
         for (int i = 0; i < studentGrade.size(); i++) {
-            if (studentGrade.get(i).getName().equals(name)) return i;
+            if (studentGrade.get(i).getId().equals(id)) return i;
         }
 //        let's say this value means the value is not found
-        return -1000;
+        return Constants.NOT_FOUND;
 
     }
 }
